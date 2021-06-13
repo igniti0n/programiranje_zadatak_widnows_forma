b60ba1409be8db80971553166357236b62a0bac9
igniti0n <ivanat735@gmail.com>
2021-06-13 18:06:51 +0300

    finished task

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.domainUpDown2 = new System.Windows.Forms.DomainUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(302, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(32, 141);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(56, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(302, 149);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(69, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "repetitive";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "number of digits:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel1.Location = new System.Drawing.Point(32, 176);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(56, 19);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel2.Location = new System.Drawing.Point(94, 176);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(56, 19);
+            this.panel2.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(94, 141);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(56, 38);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel3.Location = new System.Drawing.Point(156, 176);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(56, 19);
+            this.panel3.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(156, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(56, 38);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.panel4.Location = new System.Drawing.Point(218, 176);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(56, 19);
+            this.panel4.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(218, 141);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(56, 38);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged_1);
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(102, 27);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDown1.TabIndex = 14;
+            this.domainUpDown1.Text = "domainUpDown1";
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // domainUpDown2
+            // 
+            this.domainUpDown2.Location = new System.Drawing.Point(335, 29);
+            this.domainUpDown2.Name = "domainUpDown2";
+            this.domainUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.domainUpDown2.TabIndex = 16;
+            this.domainUpDown2.Text = "domainUpDown2";
+            this.domainUpDown2.SelectedItemChanged += new System.EventHandler(this.domainUpDown2_SelectedItemChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "number of rights";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "time remaning:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "digits left to guess:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(153, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "00 : 00";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(153, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "0";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 245);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.domainUpDown2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.DomainUpDown domainUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const int TIME_FOR_ONE_DIGIT = 10;
+        public int[] numbers;
+        public int guessedNumbers = 0;
+        public int numberToGuess;
+
+        public int s = 0;
+        public int m = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            numbers = new int[4];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int[] _domain1Items = { 4, 3, 2, 1 };
+            domainUpDown1.Items.AddRange(_domain1Items);
+            domainUpDown1.Text = "1";
+            domainUpDown2.Items.Clear();
+            domainUpDown2.Items.Add(1);
+            domainUpDown2.Text = "1";
+            timer1.Interval = 1000;
+            _switchAreSettingEnabled(true);
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _applyChangedForChangedTextAndItsPanel(panel1, textBox1, numbers[0]);
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            _applyChangedForChangedTextAndItsPanel(panel2, textBox2, numbers[1]);
+
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+            _applyChangedForChangedTextAndItsPanel(panel3, textBox3, numbers[2]);
+        }
+
+        private void textBox4_TextChanged_1(object sender, EventArgs e)
+        {
+            _applyChangedForChangedTextAndItsPanel(panel4, textBox4, numbers[3]);
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            _adjustMamixumAllowedNeededRights();
+
+        }
+
+
+
+        private void domainUpDown2_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _generateRandomNumberToGuess();
+            timer1.Start();
+            _switchAreSettingEnabled(false);
+     
+            label6.Text = (int.Parse(domainUpDown2.Text) - guessedNumbers).ToString();
+
+            s = TIME_FOR_ONE_DIGIT * int.Parse(domainUpDown1.Text);
+
+        }
+
+        private void _generateRandomNumberToGuess()
+        {
+            Random _rand = new Random();
+            int _generatedNumbers = 0;
+            int _charSpot = 1;
+            numberToGuess = 0;
+            while (_generatedNumbers < int.Parse(domainUpDown1.Text))
+            {
+                int _newNumber = _rand.Next(0, 9);
+                if (!numbers.Contains(_newNumber) || checkBox1.Checked)
+                {
+                    Console.WriteLine("generated: " + _newNumber.ToString());
+                    numbers[3 - _generatedNumbers] = _newNumber;
+                    numberToGuess += _newNumber * _charSpot;
+                    _charSpot *= 10;
+                    Console.WriteLine("number to guess: " + numberToGuess.ToString());
+                    _generatedNumbers++;
+                }
+            }
+            foreach (int number in numbers)
+            {
+                Console.WriteLine("numbers: " + number.ToString());
+            }
+        }
+
+        private void _adjustMamixumAllowedNeededRights()
+        {
+            domainUpDown2.Items.Clear();
+            List<int> newRange = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i >= int.Parse(domainUpDown1.Text))
+                {
+
+                    break;
+                }
+                newRange.Add(i + 1);
+            }
+            newRange.Reverse();
+            domainUpDown2.Items.AddRange(newRange);
+            domainUpDown2.Text = "1";
+        }
+
+
+        private void _switchAreSettingEnabled(bool areEnabled)
+        {
+            if (!areEnabled)
+            {
+                _enableFieldsBasedOnCurrentDomainValue();
+            }
+            else
+            {
+                _disableAreInputTextFields();
+            }
+            button1.Enabled = areEnabled;
+            domainUpDown1.Enabled = areEnabled;
+            domainUpDown2.Enabled = areEnabled;
+            checkBox1.Enabled = areEnabled;
+        }
+
+
+        private void _disableAreInputTextFields()
+        {
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            textBox3.Enabled = false;
+            textBox4.Enabled = false;
+            panel1.BackColor = Color.Orange;
+
+            panel2.BackColor = Color.Orange;
+
+            panel3.BackColor = Color.Orange;
+
+            panel4.BackColor = Color.Orange;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+        private void _applyChangedForChangedTextAndItsPanel(Panel panel, TextBox textBox, int numberToGuess)
+        {
+            try
+            {
+                if (textBox.Text != "")
+                {
+                    _changePanelToApproptiateColorAndCheckWin(panel, textBox, numberToGuess);
+
+                }
+
+            }
+            catch
+            {
+                panel.BackColor = Color.Orange;
+                MessageBox.Show("Entered text could not be parsed into a valid number, please enter valid numbers into the text field.");
+                textBox.Text = "";
+            }
+        }
+
+        private void _changePanelToApproptiateColorAndCheckWin(Panel panel, TextBox textBox, int numberToGuess)
+        {
+            int _newNUmber = int.Parse(textBox.Text);
+
+            if (_newNUmber == numberToGuess)
+            {
+
+                if (panel.BackColor != Color.Blue)
+                {
+                    guessedNumbers++;
+                }
+                panel.BackColor = Color.Blue;
+                if (guessedNumbers == int.Parse(domainUpDown2.Text))
+                {
+                    _winGame();
+
+                }
+
+            }
+            else
+            {
+                if (panel.BackColor == Color.Blue)
+                {
+                    guessedNumbers--;
+                }
+
+                if (numbers.Contains(_newNUmber))
+                {
+                    panel.BackColor = Color.Red;
+                }
+                else
+                {
+                    panel.BackColor = Color.Orange;
+                }
+            }
+            label6.Text = (int.Parse(domainUpDown2.Text) - guessedNumbers).ToString();
+        }
+
+        private void _winGame()
+        {
+            timer1.Stop();
+            _switchAreSettingEnabled(true);
+            _showGameEndDialog("CONGRATULATIONS!");
+        }
+
+        private void _showGameEndDialog(String message)
+        {
+            DialogResult res = MessageBox.Show(message + "\nContinue game?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+            if (res == DialogResult.No)
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        private void _enableFieldsBasedOnCurrentDomainValue()
+        {
+            String _currentValue = domainUpDown1.Text;
+            if (_currentValue == "1")
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = false;
+                textBox4.Enabled = true;
+            }
+            else if (_currentValue == "2")
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+
+            }
+            else if (_currentValue == "3")
+            {
+                textBox1.Enabled = false;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+
+            }
+            else
+            {
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            s--;
+            label5.Text = m.ToString().PadLeft(2, '0') + " : " + s.ToString().PadLeft(2, '0');
+            if (s == 0)
+            {
+                timer1.Stop();
+                _showGameEndDialog("YOU DID NOT KNOW, GAME OVER - NUMBER: " + _showUserInputNumber() + " should have been " + numberToGuess.ToString() + ".");
+                _switchAreSettingEnabled(true);
+            }
+        }
+
+        String _showUserInputNumber()
+        {
+            String _userInput = "";
+            if (textBox1.Text != "") _userInput += textBox1.Text;
+            if (textBox2.Text != "") _userInput += textBox2.Text;
+            if (textBox3.Text != "") _userInput += textBox3.Text;
+            if (textBox4.Text != "") _userInput += textBox4.Text;
+            return _userInput;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
